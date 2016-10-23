bedfbbfe74f49fec668b17a3b33f8c0662a30bcb
Sitram <adrian.martis@gmail.com>
2016-10-23 23:44:52 +0300

    Add solution to problem 4. Might still need some optimization

--- 0004_largest_palindrome_product.c
+++ 0004_largest_palindrome_product.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
 int check_palindrom(int a){
-   int i;
+   int i = 0, n = 0, found = 1;
+   int digits[10];
 
-   for (i=0; i<
+   //Save the digits of a into an array
+   while (a != 0) {
+      digits[i] = a % 10;
+      a = a / 10;
+      i++;
+   }
+
+   n = i;
+
+  for (i = 0; i<n/2 && found == 1; i++){
+     if (digits[i] != digits[n-i-1]){
+        found = 0;
+     }
+  }
+
+  return found;
 }
 
 int main(void){
    int i,j, max_palindrom = 0;
 
+   //Go over all unique numbers made by the product of 3 digits numbers
    for (i = 100; i<999; i++){
       for (j = i; j<999; j++){
-         if (check_palindrome(i*j) == 0){
-            printf("Found palindrom: %d/n", i*j);
+         if (check_palindrom(i*j) == 1){
             if (max_palindrom < i*j){
                max_palindrom = i*j;
             }
+         }
       }
    }
+
+   printf("Largest palindrom number found is %d\n", max_palindrom);
+
    return 0;
 }
