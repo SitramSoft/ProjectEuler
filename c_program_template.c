a7bf70ee490482914c124a6dc3d8e70ba0fc8ee5
Sitram <adrian.martis@gmail.com>
2019-03-14 12:33:37 +0200

    Reworked the time calculation do display time in s, ms and us in the C file template. Compiler switch TIME_MANAGEMENT has to be activated at compile time

--- c_program_template.c
+++ c_program_template.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#ifdef TIME_MEASUREMENT
 #include <time.h>
+#endif
+
 
 int main(){
-    clock_t begin = clock();
+#ifdef TIME_MEASUREMENT
+    struct timespec start, end;
+	long long delta_ms, delta_s, delta_us;
+#endif
 
+#ifdef TIME_MEASUREMENT	
+	clock_gettime(CLOCK_MONOTONIC, &start);
     /* Insert code below for measurement*/
+#endif
 
+
+
+#ifdef TIME_MEASUREMENT	
     /* End code measurement */
 
-    clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    clock_gettime(CLOCK_MONOTONIC, &end);
+	delta_s = end.tv_sec - start.tv_sec;
+	delta_ms = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+	delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 
-    printf("\n\nExecution time: %g", time_spent);
+    printf("\n\nExecution time:\n%lld s\n%lld ms\n%lld us\n", delta_s, delta_ms, delta_us);
+#endif
 
     return 0;
 }
