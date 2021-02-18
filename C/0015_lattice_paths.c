a3d944962587d568b3c05c2adc171e30231a5403
sitram <adrian.martis@gmail.com>
2021-02-18 08:11:44 +0200

    Prepared framework for solution to 15 in C

---
+++ C/0015_lattice_paths.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "config.h"
+#ifdef TIME_MEASUREMENT
+#include <time.h>
+#endif
+
+
+int main(){
+#ifdef TIME_MEASUREMENT
+    struct timespec start, end;
+	long long delta_ms, delta_s, delta_us;
+#endif
+
+#ifdef TIME_MEASUREMENT	
+	clock_gettime(CLOCK_MONOTONIC, &start);
+    /* Insert code below for measurement*/
+#endif
+
+
+
+#ifdef TIME_MEASUREMENT	
+    /* End code measurement */
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+	delta_s = end.tv_sec - start.tv_sec;
+	delta_ms = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+	delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+
+    printf("\n\nExecution time:\n%lld s\n%lld ms\n%lld us\n\n", delta_s, delta_ms, delta_us);
+#endif
+
+    return 0;
+}
